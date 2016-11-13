b36b0a51bcc07589b3e12ead6eac2dd52d417f58
Elijah Ellis <smurftown2@gmail.com>
2016-11-13 14:57:49 -0500

    Foo

--- Homework3/src/Inventory.java
+++ Homework3/src/Inventory.java
@@ -3,6 +3,9 @@
  * Date: Monday October 31 2016
  * Description: Item class for Inventory Tracker
  * */
+import java.io.*;
+import java.util.*;
+
 public class Inventory {
 
   /**
@@ -56,11 +59,48 @@ public class Inventory {
   }
 
   public void saveInventoryToFile(String fileName) {
+    // Wrap this in a try catch because we are writing to a file
+  	try {
+  		// Open the file for writing
+  		PrintWriter outputWriter = new PrintWriter(fileName);
 
+  		// For each item in the itemArray
+  		for (int i = 0; i < itemArray.length; i++) {
+  			// Write the properties of the object to a file 
+  			outputWriter.println(itemArray[i].getName());
+  			outputWriter.println(itemArray[i].getPrice());
+  			outputWriter.println(itemArray[i].getQuantity());
+  			outputWriter.println(itemArray[i].getUpc());
+  			
+  			// Append a newline to be used a delimiter when reading 
+  			outputWriter.println("");
+  		}
+  		
+  		// Close the output stream 
+  		outputWriter.close();
+  		
+  		// Alert that saving was successful
+  		System.out.println("Successfully saved inventory to " + fileName);
+  		
+
+  	} catch (Exception e) {
+  		System.out.println("An error occured why trying to write to the file");
+  	}
   }
 
   public void loadInventoryFromFile(String fileName) {
-
+	try {
+		File inputFile = new File(fileName)
+		Scanner inputReader = new Scanner(inputFile);
+		
+		// Until the end of the file 
+		while(inputReader.hasNext()) {
+			inputReader.
+		}
+		
+	} catch (Exception e) {
+		System.out.println("That file does not exist");
+	}
   }
 
 }
